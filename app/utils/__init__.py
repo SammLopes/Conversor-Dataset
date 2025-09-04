d87ab517c923ba41b06dcd3744a06a2dd7197a29
SammLopes <sam.v.p.lopes@gmail.com>
2025-09-03 22:36:45 -0300

    Implementado paths.py e __init__.py

---
+++ app/utils/__init__.py
@@ -0,0 +1,4 @@
+# utils/__init__.py
+
+# Este arquivo apenas declara utils como um pacote Python.
+# Pode ser deixado vazio por enquanto.
---
+++ app/utils/paths.py
@@ -0,0 +1,24 @@
+# app/utils/paths.py
+
+# Caminhos principais organizados para uso centralizado no projeto
+
+RAW_UNFORMATTED = "./datasets/datasets_unformat"
+YOLO_COPY = "./yolov8-copy"
+YOLO_BALANCED = "./yolov8-balanced"
+YOLO_EXTRA = "./yolov8-extra"
+CUSTOM_DATASET = "./dataset_custom"
+
+OUTPUT_DATASETS = {
+    "dataset_01": "./output_dataset_01",
+    "dataset_02": "./output_dataset_02",
+    "dataset_03": "./output_dataset_03",
+    "dataset_04": "./output_dataset_04",
+}
+
+YOLO_YAML_DATASETS = {
+    "dataset_yaml_01": "./datasets/dataset_yolo/dataset_yaml_01",
+    "dataset_yaml_02": "./datasets/dataset_yolo/dataset_yaml_02",
+    "dataset_yaml_03": "./datasets/dataset_yolo/dataset_yaml_03",
+}
+
+CLASSES_PADRAO = ["Hemorrhagic Stroke", "Ischemic Stroke", "Normal"]
