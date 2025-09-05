b153e6ae839c9011896bcb952af9f278cbfd6a0a
SammLopes <sam.v.p.lopes@gmail.com>
2025-09-04 22:29:30 -0300

    Alterado Caminho do main.py

--- app/main.py
+++ app/main.py
@@ -18,124 +18,125 @@ def main():
 
     #dataset 04
     classes_P_04 = ['Bleeding', 'Ischemia', 'Normal'];
-    gerar_labels_multiclasse('./datasets/datasets_unformat/dataset_04/train', 'output_dataset_04', classes_P_04, split='train', gerar_yaml=True);
-    gerar_labels_multiclasse('./datasets/datasets_unformat/dataset_04/test', 'output_dataset_04', classes_P_04, split='test', gerar_yaml=True);
-    gerar_labels_multiclasse('./datasets/datasets_unformat/dataset_04/valid', 'output_dataset_04', classes_P_04, split='valid', gerar_yaml=True);
+    gerar_labels_multiclasse('./data/raw/datasets_unformat/dataset_04/train', 'output_dataset_04', classes_P_04, split='train', gerar_yaml=True);
+    gerar_labels_multiclasse('./data/raw/datasets_unformat/dataset_04/test', 'output_dataset_04', classes_P_04, split='test', gerar_yaml=True);
+    gerar_labels_multiclasse('./data/raw/datasets_unformat/dataset_04/valid', 'output_dataset_04', classes_P_04, split='valid', gerar_yaml=True);
 
     #dataset 03 
     classes_P_03 = ['Normal', 'Stroke']
-    gerar_labels_multiclasse('./datasets/datasets_unformat/dataset_03/train', 'output_dataset_03', classes_P_03, split='train', gerar_yaml=True);
-    gerar_labels_multiclasse('./datasets/datasets_unformat/dataset_03/test', 'output_dataset_03', classes_P_03, split='test', gerar_yaml=True);
-    gerar_labels_multiclasse('./datasets/datasets_unformat/dataset_03/valid', 'output_dataset_03', classes_P_03, split='valid', gerar_yaml=True);
+    gerar_labels_multiclasse('./data/raw/datasets_unformat/dataset_03/train', 'output_dataset_03', classes_P_03, split='train', gerar_yaml=True);
+    gerar_labels_multiclasse('./data/raw/datasets_unformat/dataset_03/test', 'output_dataset_03', classes_P_03, split='test', gerar_yaml=True);
+    gerar_labels_multiclasse('./data/raw/datasets_unformat/dataset_03/valid', 'output_dataset_03', classes_P_03, split='valid', gerar_yaml=True);
 
     #dataset 02
     classes_P_02 = ['Hemorrhagic', 'Ischemic', 'Normal']
-    gerar_labels_multiclasse('./datasets/datasets_unformat/dataset_02/train', 'output_dataset_02', classes_P_02, split='train', gerar_yaml=True);
-    gerar_labels_multiclasse('./datasets/datasets_unformat/dataset_02/test', 'output_dataset_02', classes_P_02, split='test', gerar_yaml=True);
-    gerar_labels_multiclasse('./datasets/datasets_unformat/dataset_02/valid', 'output_dataset_02', classes_P_02, split='valid', gerar_yaml=True);
+    gerar_labels_multiclasse('./data/raw/datasets_unformat/dataset_02/train', 'output_dataset_02', classes_P_02, split='train', gerar_yaml=True);
+    gerar_labels_multiclasse('./data/raw/datasets_unformat/dataset_02/test', 'output_dataset_02', classes_P_02, split='test', gerar_yaml=True);
+    gerar_labels_multiclasse('./data/raw/datasets_unformat/dataset_02/valid', 'output_dataset_02', classes_P_02, split='valid', gerar_yaml=True);
 
     #dataset 01
     classes_P_01 = ['Normal', 'Stroke']
-    gerar_labels_multiclasse('./datasets/datasets_unformat/dataset_01/train', 'output_dataset_01', classes_P_01, split='train', gerar_yaml=True);
-    gerar_labels_multiclasse('./datasets/datasets_unformat/dataset_01/test', 'output_dataset_01', classes_P_01, split='test', gerar_yaml=True);
-    gerar_labels_multiclasse('./datasets/datasets_unformat/dataset_01/valid', 'output_dataset_01', classes_P_01, split='valid', gerar_yaml=True);
+    gerar_labels_multiclasse('./data/raw/datasets_unformat/dataset_01/train', 'output_dataset_01', classes_P_01, split='train', gerar_yaml=True);
+    gerar_labels_multiclasse('./data/raw/datasets_unformat/dataset_01/test', 'output_dataset_01', classes_P_01, split='test', gerar_yaml=True);
+    gerar_labels_multiclasse('./data/raw/datasets_unformat/dataset_01/valid', 'output_dataset_01', classes_P_01, split='valid', gerar_yaml=True);
 
     print("\n")
     # check proporcao do dataset antes do merge 
-    check_proporcao_dataset("./yolov8-copy", classes)
+    check_proporcao_dataset("./data/processed/yolov8-copy", classes)
     print("\n")
     # ======================= Conversão de datasets Yolo para o nosso formato Yolo ==================================================
 
     #dataset yolo datset yaml 01 classe Hemorrágico - Isquemico
-    merge_datasets('Hemoragik', 'Hemorrhagic Stroke', './yolov8-copy/', './datasets/dataset_yolo/dataset_yaml_01', 'train', 'train')
-    merge_datasets('Iskemik'  , 'Ischemic Stroke'   , './yolov8-copy/', './datasets/dataset_yolo/dataset_yaml_01', 'train', 'train')
+    merge_datasets('Hemoragik', 'Hemorrhagic Stroke', './data/processed/yolov8-copy/', './data/raw/dataset_yolo/dataset_yaml_01', 'train', 'train')
+    merge_datasets('Iskemik'  , 'Ischemic Stroke'   , './data/processed/yolov8-copy/', './data/raw/dataset_yolo/dataset_yaml_01', 'train', 'train')
 
-    merge_datasets('Hemoragik', 'Hemorrhagic Stroke', './yolov8-copy/', './datasets/dataset_yolo/dataset_yaml_01', 'test', 'test')
-    merge_datasets('Iskemik'  , 'Ischemic Stroke'   , './yolov8-copy/', './datasets/dataset_yolo/dataset_yaml_01', 'test', 'test')
+    merge_datasets('Hemoragik', 'Hemorrhagic Stroke', './data/processed/yolov8-copy/', './data/raw/dataset_yolo/dataset_yaml_01', 'test', 'test')
+    merge_datasets('Iskemik'  , 'Ischemic Stroke'   , './data/processed/yolov8-copy/', './data/raw/dataset_yolo/dataset_yaml_01', 'test', 'test')
     
-    merge_datasets('Hemoragik', 'Hemorrhagic Stroke', './yolov8-copy/', './datasets/dataset_yolo/dataset_yaml_01', 'valid', 'valid')
-    merge_datasets('Iskemik'  , 'Ischemic Stroke'   , './yolov8-copy/', './datasets/dataset_yolo/dataset_yaml_01', 'valid', 'valid')
+    merge_datasets('Hemoragik', 'Hemorrhagic Stroke', './data/processed/yolov8-copy/', './data/raw/dataset_yolo/dataset_yaml_01', 'valid', 'valid')
+    merge_datasets('Iskemik'  , 'Ischemic Stroke'   , './data/processed/yolov8-copy/', './data/raw/dataset_yolo/dataset_yaml_01', 'valid', 'valid')
     
     #dataset yolo datset yaml 02 classe Hemorrágico - Isquemico
-    merge_datasets('Hemoragik', 'Hemorrhagic Stroke', './yolov8-copy/', './datasets/dataset_yolo/dataset_yaml_02', 'train', 'train')
-    merge_datasets('Iskemik'  , 'Ischemic Stroke'   , './yolov8-copy/', './datasets/dataset_yolo/dataset_yaml_02', 'train', 'train')
+    merge_datasets('Hemoragik', 'Hemorrhagic Stroke', './data/processed/yolov8-copy/', './data/raw/dataset_yolo/dataset_yaml_02', 'train', 'train')
+    merge_datasets('Iskemik'  , 'Ischemic Stroke'   , './data/processed/yolov8-copy/', './data/raw/dataset_yolo/dataset_yaml_02', 'train', 'train')
 
-    merge_datasets('Hemoragik', 'Hemorrhagic Stroke', './yolov8-copy/', './datasets/dataset_yolo/dataset_yaml_02', 'test', 'test')
-    merge_datasets('Iskemik'  , 'Ischemic Stroke'   , './yolov8-copy/', './datasets/dataset_yolo/dataset_yaml_02', 'test', 'test')
+    merge_datasets('Hemoragik', 'Hemorrhagic Stroke', './data/processed/yolov8-copy/', './data/raw/dataset_yolo/dataset_yaml_02', 'test', 'test')
+    merge_datasets('Iskemik'  , 'Ischemic Stroke'   , './data/processed/yolov8-copy/', './data/raw/dataset_yolo/dataset_yaml_02', 'test', 'test')
     
-    merge_datasets('Hemoragik', 'Hemorrhagic Stroke', './yolov8-copy/', './datasets/dataset_yolo/dataset_yaml_02', 'valid', 'valid')
-    merge_datasets('Iskemik'  , 'Ischemic Stroke'   , './yolov8-copy/', './datasets/dataset_yolo/dataset_yaml_02', 'valid', 'valid')
+    merge_datasets('Hemoragik', 'Hemorrhagic Stroke', './data/processed/yolov8-copy/', './data/raw/dataset_yolo/dataset_yaml_02', 'valid', 'valid')
+    merge_datasets('Iskemik'  , 'Ischemic Stroke'   , './data/processed/yolov8-copy/', './data/raw/dataset_yolo/dataset_yaml_02', 'valid', 'valid')
 
     #dataset yolo dataset yaml 03 classes - Ischemia
-    merge_datasets('Ischemia', 'Ischemic Stroke', './yolov8-copy/', './datasets/dataset_yolo/dataset_yaml_03', 'train', 'train')
+    merge_datasets('Ischemia', 'Ischemic Stroke', './data/processed/yolov8-copy/', './data/raw/dataset_yolo/dataset_yaml_03', 'train', 'train')
 
-    merge_datasets('Ischemia', 'Ischemic Stroke', './yolov8-copy/', './datasets/dataset_yolo/dataset_yaml_03', 'test', 'test')
+    merge_datasets('Ischemia', 'Ischemic Stroke', './data/processed/yolov8-copy/', './data/raw/dataset_yolo/dataset_yaml_03', 'test', 'test')
 
-    merge_datasets('Ischemia', 'Ischemic Stroke', './yolov8-copy/', './datasets/dataset_yolo/dataset_yaml_03', 'valid', 'valid')
+    merge_datasets('Ischemia', 'Ischemic Stroke', './data/processed/yolov8-copy/', './data/raw/dataset_yolo/dataset_yaml_03', 'valid', 'valid')
 
     #output dataset 01 - classe Normal
-    merge_datasets('Normal', 'Normal', './yolov8-copy/', './output_dataset_01', 'train', 'train')
+    merge_datasets('Normal', 'Normal', './data/processed/yolov8-copy/', './data/processed/output_dataset_01', 'train', 'train')
 
-    merge_datasets('Normal', 'Normal', './yolov8-copy/', './output_dataset_01', 'test', 'test')
+    merge_datasets('Normal', 'Normal', './data/processed/yolov8-copy/', './data/processed/output_dataset_01', 'test', 'test')
     
-    merge_datasets('Normal', 'Normal', './yolov8-copy/', './output_dataset_01', 'valid', 'valid')
+    merge_datasets('Normal', 'Normal', './data/processed/yolov8-copy/', './data/processed/output_dataset_01', 'valid', 'valid')
 
     #output dataset 02 - classe 'Hemorrhagic', 'Ischemic', 'Normal'
-    merge_datasets('Hemorrhagic', 'Hemorrhagic Stroke', './yolov8-copy/', './output_dataset_02/', 'train', 'train')
-    merge_datasets('Ischemic'   , 'Ischemic Stroke'   , './yolov8-copy/', './output_dataset_02/', 'train', 'train')
-    merge_datasets('Normal'     , 'Normal'            , './yolov8-copy/', './output_dataset_02/', 'train', 'train')
+    merge_datasets('Hemorrhagic', 'Hemorrhagic Stroke', './data/processed/yolov8-copy/', './data/processed/output_dataset_02/', 'train', 'train')
+    merge_datasets('Ischemic'   , 'Ischemic Stroke'   , './data/processed/yolov8-copy/', './data/processed/output_dataset_02/', 'train', 'train')
+    merge_datasets('Normal'     , 'Normal'            , './data/processed/yolov8-copy/', './data/processed/output_dataset_02/', 'train', 'train')
 
-    merge_datasets('Hemorrhagic', 'Hemorrhagic Stroke', './yolov8-copy/', './output_dataset_02/', 'test', 'test')
-    merge_datasets('Ischemic'   , 'Ischemic Stroke'   , './yolov8-copy/', './output_dataset_02/', 'test', 'test')
-    merge_datasets('Normal'     , 'Normal'            , './yolov8-copy/', './output_dataset_02/', 'test', 'test')
+    merge_datasets('Hemorrhagic', 'Hemorrhagic Stroke', './data/processed/yolov8-copy/', './data/processed/output_dataset_02/', 'test', 'test')
+    merge_datasets('Ischemic'   , 'Ischemic Stroke'   , './data/processed/yolov8-copy/', './data/processed/output_dataset_02/', 'test', 'test')
+    merge_datasets('Normal'     , 'Normal'            , './data/processed/yolov8-copy/', './data/processed/output_dataset_02/', 'test', 'test')
 
-    merge_datasets('Hemorrhagic', 'Hemorrhagic Stroke', './yolov8-copy/', './output_dataset_02/', 'valid', 'valid')
-    merge_datasets('Ischemic'   , 'Ischemic Stroke'   , './yolov8-copy/', './output_dataset_02/', 'valid', 'valid')
-    merge_datasets('Normal'     , 'Normal'            , './yolov8-copy/', './output_dataset_02/', 'valid', 'valid')
+    merge_datasets('Hemorrhagic', 'Hemorrhagic Stroke', './data/processed/yolov8-copy/', './data/processed/output_dataset_02/', 'valid', 'valid')
+    merge_datasets('Ischemic'   , 'Ischemic Stroke'   , './data/processed/yolov8-copy/', './data/processed/output_dataset_02/', 'valid', 'valid')
+    merge_datasets('Normal'     , 'Normal'            , './data/processed/yolov8-copy/', './data/processed/output_dataset_02/', 'valid', 'valid')
 
     #output dataset 03 - classe 'Normal', 'Stroke' somente classe normal. 
-    merge_datasets('Normal', 'Normal', './yolov8-copy/', './output_dataset_03', 'train', 'train')
+    merge_datasets('Normal', 'Normal', './data/processed/yolov8-copy/', './data/processed/output_dataset_03', 'train', 'train')
 
-    merge_datasets('Normal', 'Normal', './yolov8-copy/', './output_dataset_03', 'test', 'test')
+    merge_datasets('Normal', 'Normal', './data/processed/yolov8-copy/', './data/processed/output_dataset_03', 'test', 'test')
     
-    merge_datasets('Normal', 'Normal', './yolov8-copy/', './output_dataset_03', 'valid', 'valid')
+    merge_datasets('Normal', 'Normal', './data/processed/yolov8-copy/', './data/processed/output_dataset_03', 'valid', 'valid')
 
     #out put dataset 04 - classe 'Bleeding', 'Ischemia', 'Normal'
-    merge_datasets('Bleeding', 'Hemorrhagic Stroke', './yolov8-copy/', './output_dataset_04/', 'train', 'train')
-    merge_datasets('Ischemia', 'Ischemic Stroke'   , './yolov8-copy/', './output_dataset_04/', 'train', 'train')
-    merge_datasets('Normal'  , 'Normal'            , './yolov8-copy/', './output_dataset_04/', 'train', 'train')
+    merge_datasets('Bleeding', 'Hemorrhagic Stroke', './data/processed/yolov8-copy/', './data/processed/output_dataset_04/', 'train', 'train')
+    merge_datasets('Ischemia', 'Ischemic Stroke'   , './data/processed/yolov8-copy/', './data/processed/output_dataset_04/', 'train', 'train')
+    merge_datasets('Normal'  , 'Normal'            , './data/processed/yolov8-copy/', './data/processed/output_dataset_04/', 'train', 'train')
 
-    merge_datasets('Bleeding', 'Hemorrhagic Stroke', './yolov8-copy/', './output_dataset_04/', 'test', 'test')
-    merge_datasets('Ischemia', 'Ischemic Stroke'   , './yolov8-copy/', './output_dataset_04/', 'test', 'test')
-    merge_datasets('Normal'  , 'Normal'            , './yolov8-copy/', './output_dataset_04/', 'test', 'test')
+    merge_datasets('Bleeding', 'Hemorrhagic Stroke', './data/processed/yolov8-copy/', './data/processed/output_dataset_04/', 'test', 'test')
+    merge_datasets('Ischemia', 'Ischemic Stroke'   , './data/processed/yolov8-copy/', './data/processed/output_dataset_04/', 'test', 'test')
+    merge_datasets('Normal'  , 'Normal'            , './data/processed/yolov8-copy/', './data/processed/output_dataset_04/', 'test', 'test')
 
-    merge_datasets('Bleeding', 'Hemorrhagic Stroke', './yolov8-copy/', './output_dataset_04/', 'valid', 'valid')
-    merge_datasets('Ischemia', 'Ischemic Stroke'   , './yolov8-copy/', './output_dataset_04/', 'valid', 'valid')
-    merge_datasets('Normal'  , 'Normal'            , './yolov8-copy/', './output_dataset_04/', 'valid', 'valid')
+    merge_datasets('Bleeding', 'Hemorrhagic Stroke', './data/processed/yolov8-copy/', './data/processed/output_dataset_04/', 'valid', 'valid')
+    merge_datasets('Ischemia', 'Ischemic Stroke'   , './data/processed/yolov8-copy/', './data/processed/output_dataset_04/', 'valid', 'valid')
+    merge_datasets('Normal'  , 'Normal'            , './data/processed/yolov8-copy/', './data/processed/output_dataset_04/', 'valid', 'valid')
 
     print("\n")
     # proporção do dataset depois do merge
-    check_proporcao_dataset("./yolov8-copy", classes)
+    check_proporcao_dataset("./data/processed/yolov8-copy", classes)
     print("\n")
     balancear_dataset(
-        dataset_root="./yolov8-copy",         # seu dataset atual
-        output_root="./yolov8-balanced",      # dataset balanceado final
-        extra_root="./yolov8-extra"           # imagens que sobraram
+        dataset_root="./data/processed/yolov8-copy",         # seu dataset atual
+        output_root="./data/balanced/yolov8-balanced",      # dataset balanceado final
+        extra_root="./data/balanced/yolov8-extra"           # imagens que sobraram
     )
+
     print('\n')
-    check_proporcao_dataset("./yolov8-balanced", classes)
+    check_proporcao_dataset("./data/balanced/yolov8-balanced", classes)
     classes = ["Hemorrhagic Stroke", "Ischemic Stroke", "Normal"]
 
     yolo_to_custom(
-        yolo_root="./yolov8-balanced",
-        output_root="./dataset_custom",
+        yolo_root="./data/balanced/yolov8-balanced",
+        output_root="./data/final/dataset_custom",
         classes=classes
     )
 
     # ======================= Pré-processamento ============================
     preprocess_dataset(
-        input_root="./dataset_custom",
-        output_root="./dataset_custom_preprocessed"
+        input_root="./data/final/dataset_custom",
+        output_root="./data/pre-processed/dataset_custom_preprocessed"
     )
     # ======================= Treinamento e Validação ============================
     # smart_fit()
