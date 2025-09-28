620c6cb7cb7d0e755258cf66d2be8ea638ebe22c
SammLopes <sam.v.p.lopes@gmail.com>
2025-09-28 18:04:27 -0300

    Adicionado a integração com tqdm e adicinado argumentos de epochas e batch_size como parametros da função.

--- app/core/sdac_avc/treino_sdac_avc.py
+++ app/core/sdac_avc/treino_sdac_avc.py
@@ -6,7 +6,7 @@ from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from keras.utils import to_categorical
 from tqdm.keras import TqdmCallback
 
-def train_sdavc_kfold(X, y, save_dir="modelos/sdavc", n_splits=5):
+def train_sdavc_kfold(X, y, save_dir="modelos/sdavc", n_splits=5, epochs=100, batch_size=32):
     os.makedirs(save_dir, exist_ok=True)
     kfold = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
 
@@ -22,17 +22,20 @@ def train_sdavc_kfold(X, y, save_dir="modelos/sdavc", n_splits=5):
 
         callbacks = [
             EarlyStopping(patience=50, monitor='val_loss', restore_best_weights=True),
-            ReduceLROnPlateau(patience=25, factor=0.3, verbose=1)
+            ReduceLROnPlateau(patience=25, factor=0.3, verbose=1),
+            TqdmCallback(verbose=1)
         ]
 
         history = model.fit(
             X_train, y_train,
             validation_data=(X_val, y_val),
-            epochs=100,
-            batch_size=32,
+            epochs=epochs,
+            batch_size=batch_size,
             callbacks=callbacks,
-            verbose=0
+            verbose=1
         )
 
         model.save(os.path.join(save_dir, f"sdavc_fold{fold}.h5"))
         fold += 1
+    
+    print(" Fim do treino em K-fold ")
