a732bcbbd6efda8090616551021a1898dcfd582f
SammLopes <sam.v.p.lopes@gmail.com>
2025-10-29 22:56:29 -0300

    Alterado de Flatten para GlobalAvaregePoolign2D()

--- app/core/sdac_avc/modelo_sdac.py
+++ app/core/sdac_avc/modelo_sdac.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization, Input
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization, Input, GlobalAveragePooling2D
 from keras.optimizers import Adam
 
 def build_sdavc_model(input_shape=(224, 224, 1), num_classes=3, dropout_rate=0.3, lr=0.0005):
@@ -13,7 +13,12 @@ def build_sdavc_model(input_shape=(224, 224, 1), num_classes=3, dropout_rate=0.3
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(dropout_rate))
 
-    model.add(Flatten())
+    # --- CORREÇÃO AQUI ---
+    # Substitua Flatten() pelo GlobalAveragePooling2D()
+    # model.add(Flatten()) # <-- Causa do erro de memória
+    model.add(GlobalAveragePooling2D()) # <-- Solução leve
+    # ---------------------
+
     model.add(Dense(128, activation='relu'))
     model.add(Dropout(dropout_rate))
     model.add(Dense(num_classes, activation='softmax'))
