eab2f09519ae3b83e5f5e655eba86ce86fd06b74
SammLopes <sam.v.p.lopes@gmail.com>
2025-11-16 11:46:25 -0300

    feat:[Core  | Config]:  Adicionado __init__ para transformar o effresnet_vit/ em pacote. E adicionado config para o modelo.

---
+++ app/core/effresnet_vit/__init__.py
@@ -0,0 +1,9 @@
+from .modelo_effresnet_vit import criar_modelo_effresnet_vit
+from .treino_effresnet_vit import treinar_modelo_effresnet_vit
+from .avaliador_effresnet_vit import avaliar_modelo_effresnet_vit
+
+__all__ = [
+    "criar_modelo_effresnet_vit",
+    "treinar_modelo_effresnet_vit",
+    "avaliar_modelo_effresnet_vit",
+]
