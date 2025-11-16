e6e22966a7ca6f404722ba5891aa1d96f4a5dcfd
SammLopes <sam.v.p.lopes@gmail.com>
2025-11-16 17:29:51 -0300

    fix:[Effresnet]: Concertado importações do arquivos __init__.

--- app/core/effresnet_vit/__init__.py
+++ app/core/effresnet_vit/__init__.py
@@ -1,6 +1,6 @@
 from .modelo_effresnet_vit import criar_modelo_effresnet_vit
-from .treino_effresnet_vit import treinar_modelo_effresnet_vit
-from .avaliador_effresnet_vit import avaliar_modelo_effresnet_vit
+from .treinar_modelo_effresnet_vit import treinar_modelo_effresnet_vit
+from .avaliar_modelo_effresnet_vit import avaliar_modelo_effresnet_vit
 
 __all__ = [
     "criar_modelo_effresnet_vit",
