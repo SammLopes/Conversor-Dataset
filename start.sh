464e6cffec8691acfdf79dba61d3b3f289fbc2cc
SammLopes <sam.v.p.lopes@gmail.com>
2025-11-21 12:53:27 -0300

    fix: [core]: Consertdo comando do start.sh para poder avaliar o modelo treinado do keras.

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ set -e
 
 python -m app.effresnet_vit treinar --config config/effresnet_vit_config.json
 
-python -m effresnet_vit avaliar \
-  --modelo modelos/melhor_modelo_effresnet_vit.keras \
-  --validacao dataset_custom_preprocessed/validation
+python -m app.effresnet_vit avaliar \
+  --modelo modelos/effresnet_vit/melhor_modelo_effresnet_vit.keras \
+  --validacao data/datasets_training/dataset_custom_preprocessed/valid
